7710a84d081bbf608b8a274a0b94682f61560055
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2024-09-13 18:56:34 +0100

    more options

--- R/main.R
+++ R/main.R
@@ -3,14 +3,48 @@
 #' Main function.
 #'
 #' @param path Path to the input file.
+#' @param return.type Type of image to return.
+#' @param extras List of options for sub-functions.
 #'
 #' @return A `ggplot` object.
 #' @export
-#' @importFrom magick image_read
+#' @importFrom magick image_read image_flatten image_data
 #'
 #' @examples
-#' spatialise(system.file(package = "spatialist", "Kevin.jpg"))
-spatialise <- function(path) {
+#' spatialise(
+#'   path = system.file(package = "spatialist", "Kevin.jpg")
+#' )
+#' 
+#' 
+#' spatialise(
+#'   path = system.file(package = "spatialist", "Kevin.jpg"),
+#'   extras = list(
+#'     image_flatten = list(operator = "Threshold")
+#'   )
+#' )
+spatialise <- function(path, return.type = c("raw", "flatten"), extras = list()) {
+  return.type <- match.arg(return.type)
+  
+  if (! "image_flatten" %in% names(extras)) {
+    extras$image_flatten <- list(operator = "Modulate")
+  }
+  
   img_raw <- image_read(path = path)
+  
+  if (return.type == "raw") {
+    return(img_raw)
+  }
+  
+  img_flat <- image_flatten(img_raw, extras$image_flatten$operator)
+  
+  if (return.type == "flatten") {
+    return(img_flat)
+  }
+  
+  img_data <- image_data(img_flat)
+  return(img_data)
+  # data
+  non_white <- data[1, ,] != "ff"
+  storage.mode(non_white) <- "integer"
   img_raw
 }
