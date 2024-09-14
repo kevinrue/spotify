a2827b94a26163d273a81dd8e3a46b319d06e32d
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2024-09-14 21:48:30 +0100

    fix example

--- R/point.R
+++ R/point.R
@@ -7,7 +7,7 @@
 #' @importFrom ggplot2 ggplot aes geom_point coord_fixed
 #'
 #' @examples
-#' make_point(data.frame(row = 1, column = 1))
+#' make_point(data.frame(x = 1, y = 1))
 make_point <- function(xy_coord) {
   ggplot(xy_coord, aes(x, y)) +
     geom_point() +
